b81c6868dc04e507c289f7e0d55064a7e10988d6
Joe DAlessandro <joe@thejoeshow.net>
2012-01-28 20:23:23 -0500

    oops lost the db connection info.

--- src/songmap/dbdef.clj
+++ src/songmap/dbdef.clj
@@ -1,6 +1,13 @@
 (ns songmap.dbdef
   (:use somnium.congomongo))
- 
+
+;; define our database connection
+;; this namespace can be included in
+;; whatever models i define (awesome!)
+(def db-conn (make-connection "app2658583"
+:host "staff.mongohq.com"
+:port 10028))
+
 ;; initialize mongo
 (defn init-db []
   (println ">>> initializing Mongo DB connection...")
