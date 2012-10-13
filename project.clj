69a70992c9d1202ab60a4348c1d298a8ff82dfa5
Joe DAlessandro <joe@thejoeshow.net>
2012-10-13 10:50:42 -0400

    added image scaling to track submission.

--- project.clj
+++ project.clj
@@ -6,6 +6,6 @@
                            [prxml "1.3.0"]
                            [clj-http "0.5.0"]
                            [http.async.client "0.4.5"]
-                           [org.clojure/data.json "0.1.2"]]
+                           [org.clojure/data.json "0.1.2"]
+                           [org.imgscalr/imgscalr-lib "4.2"]]
             :main tuneramblr.server)
-
--- src/tuneramblr/models/image.clj
+++ src/tuneramblr/models/image.clj
@@ -1,11 +1,33 @@
 (ns tuneramblr.models.image
   (:require [tuneramblr.models.util :as util]
             [monger.gridfs :as gfs])
-  (:use [monger.gridfs :only [store-file make-input-file filename content-type metadata]]))
+  (:use [monger.gridfs :only [store-file make-input-file filename content-type metadata]])
+  (:import [java.io ByteArrayOutputStream ByteArrayInputStream]
+           [javax.imageio ImageIO]
+           [org.imgscalr Scalr]))
 
 
 ;;;; crazy stuff can happen to images 
 ;;;; here.
+
+;; the desired scaled image dimension
+;; (in pixels)
+(def SCALED_IMAGE_DIMENSION 200)
+
+;; scales an image to the scaled
+;; dimensions: sd (assumed to be a square)
+(defn scale-img [img sd]
+  (let [imgBuff (ImageIO/read
+                  (new ByteArrayInputStream img))]
+    (let [baos (new ByteArrayOutputStream)]
+      (ImageIO/write
+        (Scalr/resize imgBuff sd nil)
+        "jpeg"
+        baos)
+      (.flush baos) 
+      (let [barr (.toByteArray baos)]
+        (.close baos)
+        barr))))
 
 ;; add an image to the store
 ;; it is assumed that the image is 
@@ -15,7 +37,9 @@
   (when (:img song)
     (let [iname (util/get-iname song)]
       (if (store-file
-            (make-input-file (:img song))
+            (make-input-file 
+              (scale-img (:img song) 
+                         SCALED_IMAGE_DIMENSION))
             (filename iname)
             (metadata {:format "jpeg"})
             (content-type "image/jpeg"))
