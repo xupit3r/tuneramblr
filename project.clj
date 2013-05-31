18ff4ffe06af580a2064702fd29e0b18cd3ef3ab
Joe D'Alessandro <joe@thejoeshow.net>
2013-05-30 21:45:35 -0400

    bumped up clojure version to 1.5.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject tuneramblr "0.1.0"
             :description "your music, your world"
-            :dependencies [[org.clojure/clojure "1.4.0"]
+            :dependencies [[org.clojure/clojure "1.5.0"]
                            [noir "1.3.0-beta1"]
                            [com.novemberain/monger "1.1.1"]
                            [prxml "1.3.0"]
